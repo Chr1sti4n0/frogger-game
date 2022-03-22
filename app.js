5250989c14a82d87e174c73417d32ead3251ca5e
Chris Jimenez <chrisjimenez8118@gmail.com>
2022-03-22 16:11:42 -0700

    Added starter files. Created game grid along with starting point, ending point and obstacle spaces on grid.

---
+++ app.js
@@ -0,0 +1,17 @@
+const timeLeftDisplay = document.querySelector('#time-left')
+const resultDisplay = document.querySelector('#result')
+const StartPauseButton = document.querySelector('#start-pause-button')
+const squares = document.querySelectorAll('.grid div')
+
+
+let currentIndex=0;
+console.log(squares)
+// Function to move frog
+function moveFrog() {
+    //console.log('frog moved')
+    
+    
+
+}
+
+document.addEventListener('keyup', moveFrog)
---
+++ game.js
@@ -0,0 +1,7 @@
+
+
+const config = {
+    width: 700,
+    height: 600,
+    
+}
